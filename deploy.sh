81bfc6a2b2e74a2319099755532d6f224e2c752b
timemachine0820 <952574482@qq.com>
2021-11-27 11:37:15 +0800

    更改为timemachine0820.github.io

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:timemachine0820/timemachine0820.github.io.git main
+git push -f git@github.com:timemachine0820/timemachine0820.github.io.git main:main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
